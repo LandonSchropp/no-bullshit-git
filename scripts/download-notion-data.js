982bb535d44452f91121c664b01c737ed6125ab1
Landon Schropp <schroppl@gmail.com>
2020-09-30 14:01:04 -0500

    Update parser to work with full Notion HTML

--- scripts/download-notion-data.js
+++ scripts/download-notion-data.js
@@ -1,73 +1,11 @@
-import _ from "lodash";
-import { JSDOM } from "jsdom";
 import { fetchPage } from "./notion";
-
-// Returns the text for the H1 or H2 previous to the element. If the element is an H1 or H2, this
-// method returns the text for the element.
-function previousHeader(element) {
-  if ([ "H1", "H2", null ].includes(element.tagName)) {
-    return element.innerHTML;
-  }
-
-  return previousHeader(element.previousSibling);
-}
-
-function createDocumentFragmentFromElements(document, elements) {
-  return elements.reduce((fragment, element) => {
-    fragment.appendChild(element);
-    return fragment;
-  }, document.createDocumentFragment());
-}
-
-function parseList(element) {
-  if (!element) {
-    return element;
-  }
-
-  return Array.from(element.children).map(child => child.innerHTML);
-}
-
-function parseSection(fragment) {
-
-  // Grab the known elements
-  let header = fragment.querySelector("h1, h2");
-  let subhead = fragment.querySelector("h1 + p, h2 + p");
-  let list = fragment.querySelector("ul");
-
-  // Remove the known elements from the fragment
-  header?.remove();
-  subhead?.remove();
-  list?.remove();
-
-  // Return the data in a structured format
-  return {
-    header: header?.innerHTML,
-    subhead: subhead?.innerHTML,
-    list: parseList(list),
-    content: parseList(fragment)
-  };
-}
-
-function parseHTML(html) {
-  let document = new JSDOM(html).window.document;
-
-  // Break the document down into child elements, split them into sections and then parse the
-  // sections.
-  return _([ ...document.querySelector("body article").children ])
-    .thru(elements => {
-      return _.slice(elements, 0, _.findIndex(elements, element => element.tagName === "HR"));
-    })
-    .groupBy(previousHeader)
-    .values()
-    .map(elements => createDocumentFragmentFromElements(document, elements))
-    .map(parseSection)
-    .value();
-}
+import { parseHTML } from "./parse-notion";
 
 (async () => {
 
   // Download the landing page's HTML
   let html = await fetchPage(process.env.NOTION_API_TOKEN, process.env.NOTION_LANDING_PAGE_ID);
+  console.log(parseHTML(html));
 
   // Split the landing page into sections
   // let sections = parseHTML(html);
---
+++ scripts/parse-notion.js
@@ -0,0 +1,99 @@
+import _ from "lodash";
+import { JSDOM } from "jsdom";
+
+/**
+ * Returns the text for the H1 or H2 previous to the element. If the element is an H1 or H2, this
+ * method returns the text for the element.
+ * @param element The element whose previous header should be found.
+ * @return Returns the previous element.
+ */
+function previousHeader(element) {
+  if (_.isNil(element)) {
+    return null;
+  }
+
+  if ([ "H1", "H2", null ].includes(element.tagName)) {
+    return element.innerHTML;
+  }
+
+  return previousHeader(element.previousSibling);
+}
+
+/**
+ * Creates a document fragment containing the given elements.
+ * @param document The document where the document fragment should be created.
+ * @param elements The elements to add to the document fragment.
+ * @return Returns a document fragment.
+ */
+function createDocumentFragmentFromElements(document, elements) {
+  return elements.reduce((fragment, element) => {
+    fragment.appendChild(element);
+    return fragment;
+  }, document.createDocumentFragment());
+}
+
+/**
+ * Parses an HTMLUnorderedList element, transforming it into structured data.
+ * @param element The list element to parse.
+ */
+function parseList(element) {
+  if (!element) {
+    return element;
+  }
+
+  return Array.from(element.children).map(child => child.innerHTML);
+}
+
+/**
+ * Parses a document fragment containing elements for a section into structured data.
+ * @param fragment The document fragment to parse.
+ * @return Returns structured data representing the section.
+ */
+function parseSection(fragment) {
+
+  // Grab the known elements
+  let header = fragment.querySelector("h1, h2");
+  let subhead = fragment.querySelector("h1 + p, h2 + p");
+  let list = fragment.querySelector("ul");
+
+  // Remove the known elements from the fragment
+  header?.remove();
+  subhead?.remove();
+  list?.remove();
+
+  // Return the data in a structured format
+  return {
+    header: header?.innerHTML,
+    subhead: subhead?.innerHTML,
+    list: parseList(list),
+    content: parseList(fragment)
+  };
+}
+
+/**
+ * Parses an HTML string into structured data.
+ * @param html A string containing the HTML to parse.
+ * @return Returns structured data representing the HTML.
+ */
+export function parseHTML(html) {
+
+  // Parse the HTML into a document
+  let document = new JSDOM(html).window.document;
+
+  // Extract the content from the document
+  let content = [
+    ...Array.from(document.querySelector("header").children),
+    ...Array.from(document.querySelector(".page-body").children)
+  ];
+
+  // Remove the unused content, Split it into sections and parse them.
+  return _(content)
+    .thru(elements => {
+      return _.slice(elements, 0, _.findIndex(elements, element => element.tagName === "HR"));
+    })
+    .groupBy(previousHeader)
+    .values()
+    .map(elements => createDocumentFragmentFromElements(document, elements))
+    .map(parseSection)
+    .value();
+}
