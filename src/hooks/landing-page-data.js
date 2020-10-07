044d1eed364a3d0b9493665d316bd88cf62ec51f
Landon Schropp <schroppl@gmail.com>
2020-10-07 17:00:57 -0500

    Parse Notion content as HTML

--- src/hooks/landing-page-data.js
+++ src/hooks/landing-page-data.js
@@ -1,6 +1,24 @@
 import _ from "lodash";
 
 import landingPageData from "../../data/landing-page.json";
+import reactParse from "html-react-parser";
+
+function recursivelyParseHTML(object) {
+
+  if (_.isNil(object)) {
+    return object;
+  }
+
+  if (_.isArray(object)) {
+    return _.map(object, recursivelyParseHTML);
+  }
+
+  if (_.isObject(object)) {
+    return _.mapValues(object, recursivelyParseHTML);
+  }
+
+  return reactParse(object);
+}
 
 /**
  * A helper hook that loads data for a section from the downloaded landing page data.
@@ -8,5 +26,6 @@ import landingPageData from "../../data/landing-page.json";
  * @return The structured data for the section.
  */
 export function useSectionData(sectionRegex) {
-  return _.find(landingPageData, (section) => sectionRegex.test(section.header));
+  let content = _.find(landingPageData, (section) => sectionRegex.test(section.header));
+  return recursivelyParseHTML(content);
 }
