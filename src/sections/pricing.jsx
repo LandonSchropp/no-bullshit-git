c10c41fe0e1c21142afe02346115c3e1ca749386
Landon Schropp <schroppl@gmail.com>
2020-10-07 15:39:50 -0500

    Add Pricing section

--- src/sections/pricing.jsx
+++ src/sections/pricing.jsx
@@ -7,5 +7,28 @@ export function Pricing() {
 
   return <section className="pricing">
     <h2 className="pricing__header">{ data.header }</h2>
+    <p className="pricing__guarantee">{ data.guarantee }</p>
+    <p className="pricing__discount">{ data.discount }</p>
+
+    <div className="pricing__tiers tiers">
+      {
+        data.tiers.map(tier => {
+          return <div className="tier" key={ tier.header }>
+            <h3 className="tier__header">{ tier.header }</h3>
+            <p className="tier__price">
+              <strike className="tier__regular-price">{ tier.price }</strike>
+              { " " }
+              <span className="tier__discounted-price">{ tier.discountedPrice }</span>
+            </p>
+            <ul>
+              {
+                data.dimensions.map(dimension => <li key={ dimension }>{ dimension }</li>)
+              }
+            </ul>
+            <button type="button">Purchase</button>
+          </div>;
+        })
+      }
+    </div>
   </section>;
 }
