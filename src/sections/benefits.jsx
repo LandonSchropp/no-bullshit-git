939e5b9676a851cedde8efe5c86eb1cc6f8fa609
Landon Schropp <schroppl@gmail.com>
2020-10-14 16:35:47 -0500

    Style the benefits section

--- src/sections/benefits.jsx
+++ src/sections/benefits.jsx
@@ -6,16 +6,23 @@ export function Benefits() {
   let data = useSectionData(/getting/i);
 
   return <section className="benefits">
-    <h2 className="benefits__header">{ data.header }</h2>
-    <p className="benefits__subhead">{ data.subhead }</p>
+    <h2>{ data.header }</h2>
+    <p className="subhead">{ data.subhead }</p>
 
-    {
-      data.list.map(item => {
-        return <div key={ item.header } className="benefit">
-          <h3 className="benefit__header">{ item.header }</h3>
-          <p className="benefit__content">{ item.content }</p>
-        </div>;
-      })
-    }
+    <div className="benefits__list">
+      {
+        data.list.map(item => {
+          return <div key={ item.header } className="benefit">
+            <img
+              className="benefit__image"
+              src="http://www.fillmurray.com/500/500"
+              alt={ item.header }
+            />
+            <h3 className="benefit__header">{ item.header }</h3>
+            <p className="benefit__content">{ item.content }</p>
+          </div>;
+        })
+      }
+    </div>
   </section>;
 }
