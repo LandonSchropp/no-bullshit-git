e0609d948a5a6b2d615e033334bb2cafbd50aae3
Landon Schropp <schroppl@gmail.com>
2020-10-14 14:57:32 -0500

    Style the Hero section

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import { Footer } from "./footer";
-import { Header } from "./header";
 
 import "normalize.css";
 import "../stylesheets/index.sass";
@@ -11,7 +10,6 @@ export function Layout({ children }) {
   return (
     <>
       <main>
-        <Header />
         { children }
         <Footer />
       </main>
--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import bull from "../images/icons/bull.svg";
+
 import { useSectionData } from "../hooks/landing-page-data";
 
 export function Hero() {
   let data = useSectionData(/no bullshit/i);
 
   return <header className="hero">
+    <img className="hero__logo" src={ bull } alt="No Bullshit Git" />
     <h1 className="hero__header">{ data.header }</h1>
-    <p className="hero__subhead">{ data.subhead }</p>
+    <p className="subhead hero__subhead">{ data.subhead }</p>
   </header>;
 }
