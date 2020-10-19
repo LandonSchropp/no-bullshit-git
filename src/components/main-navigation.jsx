f6bd091e6957c46a858a6905b04f0d4cad1309e2
Landon Schropp <schroppl@gmail.com>
2020-10-18 20:06:38 -0500

    Add the logo to the main navigation

--- src/components/main-navigation.jsx
+++ src/components/main-navigation.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import classNames from "classnames";
 import useScrollPosition from "@react-hook/window-scroll";
 
+import { Logo } from "./logo";
+
 export function MainNavigation() {
   const scrollY = useScrollPosition();
   let className = classNames("main-navigation", { "main-navigation--scrolled": scrollY !== 0 });
 
   return <nav className={ className }>
-    <a className="main-navigation__title">No Bullshit Git</a>
+    <a className="main-navigation__title" href="/">
+      <Logo className="main-navigation__logo" />
+      No Bullshit Git
+    </a>
+
     <a className="main-navigation__link" href="#pricing">Why?</a>
     <a className="main-navigation__link" href="#pricing">Benefits</a>
     <a className="main-navigation__link" href="#pricing">FAQ</a>
