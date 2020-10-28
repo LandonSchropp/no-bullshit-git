656d1610b9b851b11eee2b9ec71daf4481c44693
Landon Schropp <schroppl@gmail.com>
2020-10-28 14:54:11 -0500

    Break Tier out into a separate component

---
+++ src/components/tier.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classNames from "classnames";
+
+export function Tier({ tier: { header, price, discountedPrice, dimensions } }) {
+  return <div className="tier">
+    <h3 className="tier__header">{ header }</h3>
+    <p className="tier__price">
+      <strike className="tier__regular-price">{ price }</strike>
+      { " " }
+      <span className="tier__discounted-price">{ discountedPrice }</span>
+    </p>
+    <ul className="icon-list icon-list--check">
+      {
+        dimensions.map(({ text, checked }) => {
+          let className = classNames("icon-list__item", { "icon-list__item--unchecked": !checked });
+          return <li className={ className } key={ text }>{ text }</li>;
+        })
+      }
+    </ul>
+
+    { /* TODO: Figure out how to place this somewhere better. */ }
+    <div className="call-to-action">
+      <a
+        className="button"
+        data-gumroad-params="email=sahil@gumroad.com&price=10"
+        href="https://gum.co/no-bullshit-git"
+      >
+        Purchase
+      </a>
+    </div>
+  </div>;
+}
--- src/sections/pricing.jsx
+++ src/sections/pricing.jsx
@@ -1,39 +1,9 @@
 import React from "react";
-import classNames from "classnames";
 
+import { Tier } from "../components/tier";
 import { useSectionData } from "../hooks/landing-page-data";
-import shield from "../images/icons/shield.png";
 import lock from "../images/icons/lock.png";
-
-function Tier({ tier: { header, price, discountedPrice, dimensions } }) {
-  return <div className="tier">
-    <h3 className="tier__header">{ header }</h3>
-    <p className="tier__price">
-      <strike className="tier__regular-price">{ price }</strike>
-      { " " }
-      <span className="tier__discounted-price">{ discountedPrice }</span>
-    </p>
-    <ul className="icon-list icon-list--check">
-      {
-        dimensions.map(({ text, checked }) => {
-          let className = classNames("icon-list__item", { "icon-list__item--unchecked": !checked });
-          return <li className={ className } key={ text }>{ text }</li>;
-        })
-      }
-    </ul>
-
-    { /* TODO: Figure out how to place this somewhere better. */ }
-    <div className="call-to-action">
-      <a
-        className="button"
-        data-gumroad-params="email=sahil@gumroad.com&price=10"
-        href="https://gum.co/no-bullshit-git"
-      >
-        Purchase
-      </a>
-    </div>
-  </div>;
-}
+import shield from "../images/icons/shield.png";
 
 export function Pricing() {
   let data = useSectionData(/pricing/i);
