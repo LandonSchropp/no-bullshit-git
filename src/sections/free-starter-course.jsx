10220eb77cbddff3c4c0a92d159269f563a60f2e
Landon Schropp <schroppl@gmail.com>
2020-10-28 18:36:45 -0500

    Finish styling starter course sign up form

--- src/sections/free-starter-course.jsx
+++ src/sections/free-starter-course.jsx
@@ -7,17 +7,31 @@ import { useSectionData } from "../hooks/landing-page-data";
 export function FreeStarterCourse() {
   let data = useSectionData(/git starter course/i);
 
+  // HACK: This is a hack to insert non-breaking spaces in the header
+  let header = <>
+    The <em>Free</em> No&nbsp;Bullshit&nbsp;Git Stater Course
+  </>;
+
   return <section className="free-starter-course">
     <img className="free-starter-course__logo" src={ logo } alt="No Bullshit Git" />
 
-    <h1>{ data.header }</h1>
+    <h1 className="free-starter-course__header">{ header }</h1>
     <p className="subhead">{ data.subhead }</p>
+
+    <ul className="icon-list icon-list--white-check free-starter-course__list">
+      { data.list.map(item => <li key={ item } className="icon-list__item">{ item }</li>) }
+    </ul>
 
     <form className="call-to-action">
       <label htmlFor="email">Enter Your Email</label>
+
       <input id="email" type="email" placeholder="tomhanks@example.com" />
+
       <p className="free-starter-course__reassurance">{ data.content[0] }</p>
-      <button type="submit">Get the Course</button>
+
+      <div className="free-starter-course__submit">
+        <button type="submit">Get the Course</button>
+      </div>
     </form>
   </section>;
 }
