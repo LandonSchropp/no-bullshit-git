1588146b83c0cce2f9b715c6babe70401e80d1a6
Landon Schropp <schroppl@gmail.com>
2020-11-01 13:26:21 -0600

    Clean up the styling

--- src/components/main-navigation.jsx
+++ src/components/main-navigation.jsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import classNames from "classnames";
 import useScrollPosition from "@react-hook/window-scroll";
 
+import _ from "lodash";
+
 import { LANDING_PAGE_SECTIONS } from "../hooks/landing-page-data";
 import { Logo } from "./logo";
 import hamburger from "../images/icons/hamburger.svg";
@@ -11,26 +13,42 @@ import hamburger from "../images/icons/hamburger.svg";
 const NAVIGATIN_OFFSET = 76;
 
 function MobileNavigationItems({ onClick }) {
-  let [ , ...sections ] = LANDING_PAGE_SECTIONS;
+  let sections = _.slice(LANDING_PAGE_SECTIONS, 1).map(({ header, anchor }) => {
 
-  return sections
-    .map(({ header, anchor }) => {
+    return <AnchorLink
+      key={ header }
+      offset={ NAVIGATIN_OFFSET }
+      className="main-navigation__menu-link"
+      href={ `#${ anchor }` }
+      onClick={ onClick }
+    >
+      <img
+        className="main-navigation__menu-icon"
+        src="https://www.fillmurray.com/20/20"
+        alt={ header }
+      />
+      { header }
+    </AnchorLink>;
+  });
 
-      return <AnchorLink
-        key={ header }
-        offset={ NAVIGATIN_OFFSET }
-        className="main-navigation__menu-link"
-        href={ `#${ anchor }` }
+  return [
+    ...sections,
+    <div
+      key="get-your-copy"
+      className="main-navigation__menu-call-to-action"
+    >
+      <AnchorLink
+        className="button main-navigation__menu-button"
+        href="#pricing"
         onClick={ onClick }
       >
-        <img
-          className="main-navigation__menu-icon"
-          src="https://www.fillmurray.com/20/20"
-          alt={ header }
-        />
-        { header }
-      </AnchorLink>;
-    });
+        Get Your Copy
+      </AnchorLink>
+    </div>,
+    <p key="questions" className="main-navigation__menu-questions">
+      Questions? Reach out to <a href="mainto:schroppl@gmail.com">schroppl@gmail.com</a>.
+    </p>
+  ];
 }
 
 function DesktopNavigationItems({ onClick }) {
