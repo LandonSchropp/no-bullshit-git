52da03848f374238911bc8c5d9f916dd6da47f25
Landon Schropp <schroppl@gmail.com>
2020-11-01 20:27:19 -0600

    Improve free stater course sign up form

--- src/components/sign-up-form.jsx
+++ src/components/sign-up-form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useSectionData } from "../hooks/landing-page-data";
+import lock from "../images/icons/lock.svg";
 
 const FORM_URL = "https://landonschropp.us17.list-manage.com/subscribe/post" +
   "?u=26cdb7a71c42ac5b132805731&amp;id=6cc57c2787";
@@ -11,14 +12,14 @@ export function SignUpForm() {
   return <div className="sign-up-form">
     <h3 className="sign-up-form__header">Sign up for the Course</h3>
 
-    <ul className="icon-list icon-list--check sign-up-form__list">
-      { data.list.map(item => <li key={ item } className="icon-list__item">{ item }</li>) }
-    </ul>
-
     <form className="sign-up-form__form" action={ FORM_URL } method="post" target="_blank">
       <label htmlFor="email">Email</label>
       <input id="email" type="email" name="EMAIL" placeholder="tomhanks@example.com" />
-      <p className="sign-up-form__reassurance">{ data.content[0] }</p>
+      <p className="sign-up-form__reassurance">
+        <img className="inline-icon" src={ lock } />
+        { " " }
+        { data.content[0] }
+      </p>
 
       <div className="sign-up-form__submit">
         <button
