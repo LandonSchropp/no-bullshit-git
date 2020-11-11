9f0fb490698ba9606ffd6886971741d83613dd17
Landon Schropp <schroppl@gmail.com>
2020-11-11 15:26:02 -0600

    Switch web fonts to imports

--- src/html.js
+++ src/html.js
@@ -20,19 +20,6 @@ export default function HTML(props) {
           href="/images/favicon.png"
         />
 
-        <link
-          href={
-            "https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght" +
-              "@0,400;0,700;1,400;1,700&display=swap"
-          }
-          rel="stylesheet"
-        />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap"
-          rel="stylesheet"
-        />
-
         { props.headComponents }
       </head>
       <body { ...props.bodyAttributes } id="__gatsby">
