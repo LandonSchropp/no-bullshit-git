1b926f05f9eace50234f210b1e5c526758421272
Landon Schropp <schroppl@gmail.com>
2021-03-02 13:06:48 -0600

    Remove page content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,5 @@
-import { kebabCase } from "voca";
 import React from "react";
 
-import _ from "lodash";
-
-import { LANDING_PAGE_SECTIONS } from "../hooks/landing-page-data";
-import { Layout } from "../components/layout";
-import { SEO } from "../components/seo";
-import { importHash } from "../../lib/import";
-
-const sectionModules = importHash(require.context("../sections", false));
-
-let sectionComponents = _.map(LANDING_PAGE_SECTIONS, section => {
-  let module = _.find(
-    sectionModules,
-    (_module, key) => _.includes(key, kebabCase(section.component))
-  );
-
-  return {
-    ...section,
-    reactComponent: _.head(_.values(module))
-  };
-});
-
 export default function IndexPage() {
-  return <>
-    <SEO />
-    <Layout>
-      {
-        sectionComponents.map(({ reactComponent: Component, component }) => {
-          return <Component key={ component } />;
-        })
-      }
-    </Layout>
-  </>;
+  return <h1>Hello</h1>
 }
