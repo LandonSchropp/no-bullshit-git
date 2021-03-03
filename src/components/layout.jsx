f4c74ce28d232a049019f100fc85c6a7120f0026
Landon Schropp <schroppl@gmail.com>
2021-03-03 13:22:17 -0600

    Add a mobile-cta section

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,11 +7,13 @@ import React from "react";
 
 import { Footer } from "./footer";
 import { MainNavigation } from "./main-navigation";
+import { MobileCTA } from "../sections/mobile-cta";
 
 export function Layout({ children }) {
   return <main>
     <MainNavigation />
     { children }
+    <MobileCTA />
     <Footer />
   </main>;
 }
---
+++ src/sections/mobile-cta.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function MobileCTA() {
+  return <footer className="mobile-cta">
+    Check out the
+    { " " }
+    <a className="mobile-cta__anchor" href="/free-intro-course">
+      <em>Free</em> Intro Course!
+    </a>
+  </footer>;
+}
